725c7e37586906c558b7bfe71a590ed4d1394e72
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-13 23:37:16 -0400

    passing api endpoint

--- ci/tasks/deploy-to-dev-paas.sh
+++ ci/tasks/deploy-to-dev-paas.sh
@@ -17,6 +17,6 @@ az appservice web config container update -s dev -n $server_prefix-web-nodejs -g
     --docker-custom-image-name $acr_endpoint/ossdemo/web-nodejs:$img_tag
 
 az appservice web config appsettings update --setting PORT=3000 -g $paas_rg -n $server_prefix-web-nodejs
-az appservice web config appsettings update --setting API_ENDPOINT=http://${server_prefix}-api-nodejs-dev.azurewebsites.net -g $paas_rg -n $server_prefix-web-nodejs
+az appservice web config appsettings update -s dev --setting API_ENDPOINT=http://${server_prefix}-api-nodejs-dev.azurewebsites.net -g $paas_rg -n $server_prefix-web-nodejs
 
 echo "The WEB App is available here:${server_prefix}-web-nodejs-dev.azurewebsites.net"
