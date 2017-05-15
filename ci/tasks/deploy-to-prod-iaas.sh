869da41db60c9602c4018d86885fba5b425d3d88
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-14 23:51:49 -0400

    prod deploy iaas and paas

---
+++ ci/tasks/deploy-to-prod-iaas.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+echo "Deploying to PROD PAAS"
+img_tag=$(<web-version/number)
+echo "Image version: "$img_tag
+
+echo -e "Deploy containers via ansible to worker iaas servers..."
+#change into the directory where the Ansible CFG is located
+
+mkdir ~/.ssh
+#Had to do this as the key is being read in one single line
+printf "%s\n" "-----BEGIN RSA PRIVATE KEY-----" >> ~/.ssh/id_rsa
+printf "%s\n" $server_ssh_private_key | tail -n +5 | head -n -4 >>  ~/.ssh/id_rsa
+printf "%s" "-----END RSA PRIVATE KEY-----" >> ~/.ssh/id_rsa
+echo $server_ssh_public_key >> ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa*
+
+web_repository=$acr_endpoint/ossdemo/web-nodejs:$img_tag
+api_endpoint="http://prod-${server_prefix}.${location}.cloudapp.azure.com:81"
+touch web-nodejs/ci/tasks/ansible/docker-hosts
+printf "%s\n" "[dockerhosts]" >> web-nodejs/ci/tasks/ansible/docker-hosts
+printf "%s\n" "prod-${server_prefix}.${server_location}.cloudapp.azure.com" >> web-nodejs/ci/tasks/ansible/docker-hosts
+#printf "%s\n" "staging-${server_prefix}.${server_location}.cloudapp.azure.com" >> web-nodejs/ci/tasks/ansible/docker-hosts
+
+sed -i -e "s@VALUEOF-DEMO-ADMIN-USER-NAME@${server_admin_username}@g" web-nodejs/ci/tasks/ansible/playbook-iaas-docker-deploy.yml
+sed -i -e "s@VALUEOF-REGISTRY-SERVER-NAME@${acr_endpoint}@g" web-nodejs/ci/tasks/ansible/playbook-iaas-docker-deploy.yml
+sed -i -e "s@VALUEOF-REGISTRY-USER-NAME@${acr_username}@g" web-nodejs/ci/tasks/ansible/playbook-iaas-docker-deploy.yml
+sed -i -e "s@VALUEOF-REGISTRY-PASSWORD@${acr_password}@g" web-nodejs/ci/tasks/ansible/playbook-iaas-docker-deploy.yml
+sed -i -e "s@VALUEOF-IMAGE-REPOSITORY@${web_repository}@g" web-nodejs/ci/tasks/ansible/playbook-iaas-docker-deploy.yml
+sed -i -e "s@VALUEOF-API-ENDPOINT@${api_endpoint}@g" web-nodejs/ci/tasks/ansible/playbook-iaas-docker-deploy.yml
+API_ENDPOINT=VALUEOF-API-ENDPOINT
+
+cd web-nodejs/ci/tasks/ansible
+ ansible-playbook -i docker-hosts playbook-iaas-docker-deploy.yml --private-key ~/.ssh/id_rsa
+cd ..
+
+echo -e ".you can now browse the application at http://prod-${server_prefix}.${server_location}.cloudapp.azure.com for individual servers."
+
---
+++ ci/tasks/deploy-to-prod-paas.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+
+echo "Deploying to PROD PAAS"
+img_tag=$(<web-version/number)
+echo "Image version: "$img_tag
+
+
+az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
+az account set --subscription "$subscription_id"
+az appservice web config container update -s prod -n $server_prefix-web-nodejs -g $paas_rg \
+    --docker-registry-server-password $acr_password \
+    --docker-registry-server-user $acr_username \
+    --docker-registry-server-url $acr_endpoint \
+    --docker-custom-image-name $acr_endpoint/ossdemo/web-nodejs:$img_tag
+
+az appservice web config appsettings update --setting PORT=3000 -g $paas_rg -n $server_prefix-web-nodejs
+az appservice web config appsettings update -s prod --setting API_ENDPOINT=http://${server_prefix}-api-nodejs.azurewebsites.net -g $paas_rg -n $server_prefix-web-nodejs
+
+echo "The WEB App is available here:${server_prefix}-web-nodejs.azurewebsites.net"
